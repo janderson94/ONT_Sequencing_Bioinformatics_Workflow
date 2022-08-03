7b35c18c58af27c9b3f2f1a6710b5a46ad8fc308
janderson94 <jordan.a.anderson@duke.edu>
2022-08-03 11:10:28 -0400

    Fixed pileup genome.

--- pileup.sh
+++ pileup.sh
@@ -3,4 +3,5 @@
 module load samtools
 module load bcftools
 
-samtools mpileup -uf /data/tunglab/shared/genomes/panubis1_bwa/Panubis1.0.fa aligned_sam/aligned_BARCODE.bam | bcftools call -V indels -m > genotypes/BARCODE.vcf
+#Double check genome path
+samtools mpileup -uf /data/tunglab/shared/genomes/panubis1_bwa_preNCBI/Panubis1.0.fa aligned_sam/aligned_BARCODE.bam | bcftools call -V indels -m > genotypes/BARCODE.vcf
